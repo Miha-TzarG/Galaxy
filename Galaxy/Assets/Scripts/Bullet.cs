6c7e503fb0938ec39e76efde6f11dab3d3953c50
Miha-TzarG <67576793+Miha-TzarG@users.noreply.github.com>
2021-01-18 17:39:51 +0300

    2
    Add player(move and shoot). Add Rock and nlo (all this enemy). Destroy enemy

---
+++ Galaxy/Assets/Scripts/Bullet.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speedBullet;
+
+    public float TimeToDisable;
+    public Transform ParentPointShot;
+
+    public string tagname;
+    void OnEnable()
+    {
+     
+        StartCoroutine(SetDisabled(TimeToDisable));
+
+        ParentPointShot = GameObject.FindGameObjectWithTag(tagname).GetComponent<Transform>();
+        //this.transform.localScale = new Sc(1, 1, 1);
+        transform.localScale.Set(1f, 1f, 1f);
+
+    }
+    IEnumerator SetDisabled(float TimeToDisable)
+    {
+
+        yield return new WaitForSeconds(TimeToDisable);
+
+        gameObject.SetActive(false);
+        gameObject.transform.parent = ParentPointShot;
+        gameObject.transform.rotation = ParentPointShot.transform.rotation;
+
+    }
+    void Update()
+    {
+
+        transform.Translate(Vector3.right * speedBullet * Time.deltaTime);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "NLO")
+        {
+            collision.gameObject.SetActive(false);
+         
+            gameObject.SetActive(false);
+            Debug.Log("Check");
+            //     gameObject.transform.parent = ParentPointShot;
+            //  gameObject.transform.rotation = ParentPointShot.transform.rotation;
+
+        }
+
+        if (collision.tag == "Rock1")
+        {
+       
+            collision.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            //  gameObject.transform.parent = ParentPointShot;
+            // gameObject.transform.rotation = ParentPointShot.transform.rotation;
+
+        }
+
+        if (collision.tag == "Rock2")
+        {
+
+            collision.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            //  gameObject.transform.parent = ParentPointShot;
+            // gameObject.transform.rotation = ParentPointShot.transform.rotation;
+
+        }
+    }
+}
---
+++ Galaxy/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float Speed = 6f;
+   
+    public Rigidbody2D rb;
+    public bool a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     var mousePosition = Input.mousePosition;
+            //mousePosition.z = transform.position.z - Camera.main.transform.position.z; // это только для перспективной камеры необходимо
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); //положение мыши из экранных в мировые координаты
+            var angle = Vector2.Angle(Vector2.right, mousePosition - transform.position);//угол между вектором от объекта к мыше и осью х
+            transform.eulerAngles = new Vector3(0f, 0f, transform.position.y < mousePosition.y ? angle : -angle);//немного магии на последок
+
+       
+    }
+
+    public void FixedUpdate()
+    {
+        MovementLogic();
+
+    }
+    private void MovementLogic()
+    {
+
+    
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+           float moveVertical = Input.GetAxis("Vertical");
+
+           Vector3 movement = new Vector3(moveHorizontal,  moveVertical , 0.0f);
+       
+           rb.AddForce(movement * Speed);
+       
+        
+    }
+}
